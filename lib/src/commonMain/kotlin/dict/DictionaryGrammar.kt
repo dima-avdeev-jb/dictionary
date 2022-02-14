16923449c9922be197a383854c8d57aaf52a437f
avdim <avdim@mail.ru>
2022-02-14 17:48:52 +0300

    grammar

--- lib/src/commonMain/kotlin/dict/DictionaryGrammar.kt
+++ lib/src/commonMain/kotlin/dict/DictionaryGrammar.kt
@@ -6,7 +6,7 @@ val dictionaryGrammar
         name = "Grammar",
         useByDefault = true,
         words = buildList {
-            word("формы be", "am/is/are, was/were")
+            word("формы be", "am/is/are, прошедшее was/were")
             word("формы have", "have/has, had")
             word("вспомогательные глаголы", "be, have, do")
             word("", "")
