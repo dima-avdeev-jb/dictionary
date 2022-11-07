54d631a90f4a88e3902dfa3763f22760c1c1e05e
dima.avdeev <dima.avdeev@jetbrains.com>
2022-11-07 11:08:25 +0400

    update

--- lib/src/commonMain/kotlin/dict/Dictionary6.kt
+++ lib/src/commonMain/kotlin/dict/Dictionary6.kt
@@ -8,6 +8,17 @@ val dictionary6
         words = buildList {
             word("opinion, opinionated", "мнение, самоуверенный упрямый")
             word("suddenly", "случайно")
+            word("dock, docking", "причал стыковка")
+            word("Yours sincerely", "искренне Ваш")
+            word("crucial", "решающий ключевой")
+            word("curious", "любопытный интересно")
+            word("float", "плавать")
+            word("comprehensive", "всесторонний, всеобъемлющий")
+            word("bound, boundary", "связанный, граница")
+            word("streamline", "упорядочить, рационализировать")
+            word("", "")
+            word("", "")
+            word("", "")
             word("", "")
         }
     )
